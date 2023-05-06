5ee5e60ac61392d677571092ea65ad5d9da58b00
Dev hacker <67338604+cyberhacker17@users.noreply.github.com>
2023-05-06 14:33:42 -0400

    Update knock.sh

--- knock.sh
+++ knock.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#Version 1.3
 #this scripts can open ports in a host
 host=172.16.1
 for ip in $(seq 1 1024);
 do
-knock -d 500 -v $host.$ip 13 37 30000 3000;
+knock -d 500 -v $host.$ip 13 37 30000 3000 40000 3456 1467;
 done
